5c3b85201676b09479070390cff49accb7c06d00
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-02 18:00:48 +0300

    Inject Splash Screen Fade In Effect

--- AndroidStudioProjects/MyApplication2/app/src/main/java/com/example/myapplication/homepage.java
+++ AndroidStudioProjects/MyApplication2/app/src/main/java/com/example/myapplication/homepage.java
@@ -2,7 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class homepage extends AppCompatActivity {
 
@@ -10,5 +15,6 @@ public class homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
     }
 }
